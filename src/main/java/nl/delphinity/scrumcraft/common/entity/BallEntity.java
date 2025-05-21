1d91ceb1118dc52ca40b80434712805ed4f4d8ef
null/ <null.ptr.exc@outlook.com>
2025-05-21 11:50:17 +0200

    Met Vlad zn voorbeeld ervoor gezorgd dat de bal nu damage en knockback doet

--- src/main/java/nl/delphinity/scrumcraft/common/entity/BallEntity.java
+++ src/main/java/nl/delphinity/scrumcraft/common/entity/BallEntity.java
@@ -1,12 +1,18 @@
 package nl.delphinity.scrumcraft.common.entity;
 
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.entity.damage.DamageTypes;
 import net.minecraft.entity.projectile.thrown.ThrownItemEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.predicate.entity.DamageSourcePredicate;
+import net.minecraft.registry.RegistryKeys;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.sound.SoundCategory;
+import net.minecraft.util.hit.EntityHitResult;
 import net.minecraft.util.hit.HitResult;
 import net.minecraft.world.World;
 import nl.delphinity.scrumcraft.init.ModEntityTypes;
@@ -39,7 +45,20 @@ public class BallEntity extends ThrownItemEntity {
 
         world.playSound(null, this.getX(), this.getY(), this.getZ(), ModSounds.BALL_HIT, SoundCategory.PLAYERS, 0.5f, 1F);
 
-        if(!world.isClient){
+        if (!world.isClient) {
+
+            if (hitResult.getType() == HitResult.Type.ENTITY) {
+                EntityHitResult entityHitResult = (EntityHitResult) hitResult;
+                Entity target = entityHitResult.getEntity();
+
+                DamageSource damageSource = world.getDamageSources().thrown(this, this.getOwner()); // or getOwner() == null for generic
+                target.damage((ServerWorld) world, damageSource, 1F);
+
+                target.addVelocity(this.getVelocity().multiply(3, 3, 3));
+                target.velocityModified = true;
+            }
+
+
             this.kill((ServerWorld) world);
         }
     }
